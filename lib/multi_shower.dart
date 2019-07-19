78e654ab8001a0552609576bce6e53973c8c6e04
mac <1981462002@qq.com>
2019-07-19 17:49:07 +0800

    Initial commit

---
+++ lib/multi_shower.dart
@@ -0,0 +1,43 @@
+library multi_shower;
+
+import 'package:flutter/material.dart';
+
+class MultiShower extends StatelessWidget {
+  MultiShower(
+      this.list,
+      this.call, {
+        this.width = 110,
+        this.height = 110 * 0.618,
+        this.infos,
+        this.color = Colors.cyanAccent,
+      });
+
+  final List list;
+  final List<String> infos;
+  final Function call;
+  final double width;
+  final double height;
+  final Color color;
+
+  @override
+  Widget build(BuildContext context) {
+    var li = <Widget>[];
+    for (var i = 0; i < list.length; i++) {
+      var child = Container(
+          margin: EdgeInsets.all(17),
+          color: color,
+          width: width,
+          height: height,
+          child: call(list[i]));
+      li.add(Column(
+        children: <Widget>[
+          child,
+          Text(infos != null ? infos[i] : list[i].toString().split(".")[1])
+        ],
+      ));
+    }
+    return Wrap(
+      children: li,
+    );
+  }
+}
